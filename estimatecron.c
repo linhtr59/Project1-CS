779d1f4378130a103ff516dead854c39d05f4c68
linhtr59 <103659319+linhtr59@users.noreply.github.com>
2022-09-12 14:28:38 +0800

    Update estimatecron.c

--- estimatecron.c
+++ estimatecron.c
@@ -7,25 +7,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 #define MAX_LEN 20
 #define MAX_CHAR 100
-#define ANY '*'
+
 
 
 struct Crontab{
-    char minute[2];
-    char hour[2];
-    char day[2];
+    char minute[3];
+    char hour[3];
+    char day[3];
     char month[3];
     char date[3];
-    char command[40];
+    char command[41];
 };
 
 
 struct Estimate{
-    char command[40];
-    int min;
+    char command[41];
+    char min[3];
 };
 
 
@@ -45,20 +47,21 @@ FILE *openfile(char filename[]) {
 
 
 
-void fileProcessCron(FILE *crontab, struct Crontab crontab_data[MAX_LEN]){
+int fileProcessCron(FILE *crontab, struct Crontab crontab_data[MAX_LEN]){
     int line = 0;//keeps track of every line in file and also act as pointer for struct idex
     int j = 0; //keeps track of the individual character in a line
+    int file_size = 0;
 
     char lines[MAX_LEN];
 
 
     while(fgets(lines, sizeof lines, crontab)!= NULL){
-        char cmin[2];
-        char chour[2];
-        char cday[2];
-        char cmonth[3];
-        char cdate[3];
-        char cmd[40];
+        char cmin[3];
+        char chour[3];
+        char cday[3];
+        char cmonth[4];
+        char cdate[4];
+        char cmd[41];
 
         while (isspace(lines[j])){ // if the character is a white space, continute to examine next character
             j ++;
@@ -74,42 +77,47 @@ void fileProcessCron(FILE *crontab, struct Crontab crontab_data[MAX_LEN]){
             strcpy(crontab_data[line].date, cdate);
             strcpy(crontab_data[line].command, cmd);
             line++;
+            file_size ++;
         }
 
     }
+    return file_size;
 
 }
 
 
 
 
-void fileProcessEstimate(struct Estimate est_data[MAX_LEN], FILE *estimate){
+int fileProcessEstimate( FILE *estimate, struct Estimate est_data[MAX_LEN]){
     int line = 0;//keeps track of every line in file and act as pointer for struct index
     int j = 0; //keeps track of the individual character in a line
+    int file_size = 0;
 
     char lines[MAX_LEN];
 
     while(fgets(lines, sizeof lines, estimate)!= NULL){
-        char cmd[40];
-        int minute = 0;
+        char cmd[41];
+        char minute[3];
+        
+
 
         while (isspace(lines[j])){ // if the character is a white space, continute to examine next character
             j ++;
         }
 
         if (lines[j] != '#'){ // if the first nons-space character is not a # then store data into struct 
-            sscanf(lines, "%s,%i",
+            sscanf(lines, "%s,%s",
                           cmd, minute);
             strcpy(est_data[line].command, cmd);
-            est_data[line].min = minute; 
+            strcpy(est_data[line].min, minute); 
             line++;
+            file_size ++;
         }
 
     }
+    return file_size;
 
 }
-
-
 
 
 
@@ -144,25 +152,28 @@ int daysOfMonth(int month){
 
 
 
-
-
 int dayToInt(char *day){
     //if given a string (mon-sun) convert that to a number
-    int num; 
-    int check = atoi(day);
-    if(!(isdigit(check))){ 
-        num = check;// if it is already an int, return that
+    int num = 0; 
+    if((isnumber(*day))){ 
+        if(atoi(day) < 7){
+            num = atoi(day);// if it is already an int that is less than 7, return that
+            return num;
+        }
+
     }
-
-    const char *days[7] = {"mon", "tue", "wed",
-                             "thu", "fri", "sat","sun"};
-
-    for(int i = 0; i < 7; i++){
-        if(strcmp(days[i], day) ==0){ //Compares whether day match with any day array item 
+    else{
+        char *days[7] = {"mon", "tue", "wed", "thu", "fri", "sat","sun"};
+        for(int i = 0; i < 7; i++){
+        if((strcmp(days[i], day)) == 0){ // Compares whether given day is a valid day (or substring)
             num = i;
+            return num;
         }
     }
-    return num;
+    }
+    
+    fprintf(stderr, "Invalid input");
+    exit(EXIT_FAILURE);
 }
 
 
@@ -172,20 +183,26 @@ int dayToInt(char *day){
 int monthToInt(char *month){
     //if given a string (jan-dec) convert that to a number
     int num; 
-    int check = atoi(month);
-    if(!(isdigit(check))){ 
-        num = check;// if it is already an int, return that
-    }
-
-    const char *months[12] = {"jan", "feb", "mar","apr", "may", 
-                            "jun","jul", "aug", "sep", "oct", "nov", "dec"};
-
-    for(int i = 0; i < 12; i++){
-        if(strcmp(months[i], month) == 0){ // Compares whether month match with any months array item
-            num = i;
+    if(isnumber(*month)){
+        if(atoi(month) < 11){
+            num = atoi(month);
+            return num;
         }
     }
-    return num;
+    else{
+        const char *months[12] = {"jan", "feb", "mar","apr", "may", 
+                            "jun","jul", "aug", "sep", "oct", "nov", "dec"};
+
+        for(int i = 0; i < 12; i++){
+        if(strcmp(months[i], month) == 0){ // Compares whether month match with any months array item
+            num = i;
+            return num;
+        }
+    }
+
+    }
+    fprintf(stderr, "Month is an invalid input");
+    exit(EXIT_FAILURE);
 
 }
    
@@ -205,13 +222,11 @@ int validDay(int month, int day){
 
 
 
+void reportError(struct Crontab crontab_data[MAX_LEN], int cronSize){
 
-
-void reportError(struct Crontab crontab_data[MAX_LEN]){
-//GETTING SOME WARNINGS HERE WITH COMPARING ANY TO THE DATA BUT MAYBE ITS FINE??? ALSO SIZEOF IS SOMEHTIMES INACCURATE, ASKED Q ON HELP FORUM, WILL GET BACK TO FIX THIS
-    for (int i =0; i < sizeof crontab_data; i ++ ){
+    for (int i =0; i < cronSize ; i ++ ){
         //reporting errors on minute in crontab,  
-        if (strcmp(crontab_data[i].minute, ANY)!= 0 ){//if minute is not * -->
+        if (strcmp(crontab_data[i].minute, "*")!= 0){//if minute is not * -->
             if(atoi(crontab_data[i].minute) < 0 || atoi(crontab_data[i].minute) > 59){//check if its < 0 or > 59, if yes report error and exit 
                 fprintf(stderr,"Minute provided is out of scope");
                 exit(EXIT_FAILURE);
@@ -220,7 +235,7 @@ void reportError(struct Crontab crontab_data[MAX_LEN]){
 
 
         //reporting errors on hour in crontab
-        if (strcmp(crontab_data[i].hour, ANY)!= 0 ){//if hour is not * -->
+        if (strcmp(crontab_data[i].hour, "*")!= 0 ){//if hour is not * -->
             if(atoi(crontab_data[i].hour) < 0 || atoi(crontab_data[i].hour) > 23){//check if its <0 or > 23, if yes report error and exit
                 fprintf(stderr,"Hour provided is out of scope");
                 exit(EXIT_FAILURE);
@@ -229,20 +244,20 @@ void reportError(struct Crontab crontab_data[MAX_LEN]){
 
 
         //reporting errors on date in crontab
-        if (strcmp(crontab_data[i].date, ANY) !=0){ //if date is not * -->
+        if (strcmp(crontab_data[i].day, "*") !=0){ //if date is not * -->
 
-            if(strcmp(crontab_data[i].month, ANY) ==0){ // if month is equals to * report generic error
-                if (atoi(crontab_data[i].date) < 0 || atoi(crontab_data[i].date > 31)){ // date < 0 or date > 31 then exit and report error
+            if(strcmp(crontab_data[i].month,"*") ==0){ // if month is equals to * report generic error
+                if (atoi(crontab_data[i].day) < 0 || atoi(crontab_data[i].day) > 31 ){ // date < 0 or date > 31 then exit and report error
                     fprintf(stderr,"Date provided is out of scope");
                     exit(EXIT_FAILURE);
 
                 }
             }
-            else if (strcmp(crontab_data[i].month, ANY) !=0){ // and if the month provided is also not * -->
+            else if (strcmp(crontab_data[i].month, "*") !=0){ // and if the month provided is also not * -->
                 int currentMonth = monthToInt(crontab_data[i].month);
-                int currentDate = dayToInt(crontab_data[i].date);
+                int maxDay = daysOfMonth(currentMonth);
                 //report error accordingly to the specified month
-                if (currentDate < 0 || (validDay != 0)){ // if the date provided is < 0 or is larger than the max date for the provide month, report error and exit 
+                if (atoi(crontab_data[i].day) < 0 || atoi(crontab_data[i].day) > maxDay ){ // if the date provided is < 0 or is larger than the max date for the provide month, report error and exit 
                     fprintf(stderr,"Date provided is out of scope");
                     exit(EXIT_FAILURE);
                 }     
@@ -252,7 +267,7 @@ void reportError(struct Crontab crontab_data[MAX_LEN]){
 
 
         //reporting errors on month in crontab
-        if (strcmp(crontab_data[i].month, ANY) !=0){ // if day is not * --> 
+        if (strcmp(crontab_data[i].month, "*") !=0){ // if day is not * --> 
             int currentMonth = monthToInt(crontab_data[i].month);
             if (currentMonth < 0 || currentMonth > 11){ // if month is < 0 or > 11, report error and exit 
                 fprintf(stderr,"Month provided is out of scope");
@@ -262,7 +277,7 @@ void reportError(struct Crontab crontab_data[MAX_LEN]){
 
 
         //reporting erros on day in crontab
-        if(strcmp(crontab_data[i].day, ANY) !=0){ //if dayis not * -->
+        if(strcmp(crontab_data[i].day, "*") !=0){ //if dayis not * -->
             int currentDay = dayToInt(crontab_data[i].month);
             if (currentDay < 0 || currentDay > 6){ // if day is < 0 or > 6, report error and exit 
                 fprintf(stderr,"Day provided is out of scope");
@@ -275,11 +290,14 @@ void reportError(struct Crontab crontab_data[MAX_LEN]){
 }
 
 
+
 void estimatecron(char *month, FILE *crontab_file, FILE *estimates_file){
-    int maxCommand = 0;
-    int invokedCommand = 0;
-    int nrunning = 0; //keeps track of no of process running at a given minute 
+    int maxCommand = 0; //max command invoked at any particular time
+    int invokedCommand = 0; //total invoked command in the month
+    int nrunning = 0; // keeps track of the amount of command running while looping through every minute
     int cmdMonth = monthToInt(month);
+
+
 
     struct Crontab crontab_data[MAX_LEN];
     struct Estimate est_data[MAX_LEN];
@@ -290,24 +308,19 @@ void estimatecron(char *month, FILE *crontab_file, FILE *estimates_file){
         int time;
     }process[MAX_LEN];
 
-    fileProcessEstimate(est_data, estimates_file);
-    fileProcessCron(crontab_data, crontab_file);
-    reportError(crontab_data);
+    int estSize = fileProcessEstimate(estimates_file, est_data );
+    int cronSize = fileProcessCron( crontab_file, crontab_data);
+    reportError(crontab_data, cronSize);
 
     //iterate through every minute in the provided and month and log commands 
     int minInMonth = (60*24) * daysOfMonth(cmdMonth); //no of minutes in the given month
     for (int min = 0; min <= minInMonth; min++){
-        for (int i =0; i < sizeof crontab_data; i++){
-// iterate through crontab lines and if the day & month and date and hour and min match approrpiately then do something
-// *** needs to somehow be able to determine if a process start in this minute then incremen nrunning if a process terminate then decrement nrunning         
-   *** at any one point nrunning become greater than maxCommand, update maxCommand
-   *** also need too somehow keep track of the most frequently invoked commands 
-   
+        for (int i =0; i < cronSize; i++){
+            
+            
         }
 
     }
-
-
 
 }
 
